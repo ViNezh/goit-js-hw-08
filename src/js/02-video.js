c712a0850b679938c480427447651180746c3730
ViNezh <126874232+ViNezh@users.noreply.github.com>
2023-07-25 19:16:25 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+        const player = new Player(iframe);
+
+        player.on('play', function() {
+            console.log('played the video!');
+        });
