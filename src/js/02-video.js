3c0535424422c8b32d5c79814ee939dff9fbfc22
ViNezh <126874232+ViNezh@users.noreply.github.com>
2023-07-26 20:25:23 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,37 @@
+// Імпорт бібліотек
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-        const player = new Player(iframe);
+const player = new Player(iframe);
+// Обробка події теперішній час при програванні відео з затримкою 1с
+player.on('timeupdate',throttle(savedTime,1000) );
+// Функція збереження часу з плейера до сховища
+function savedTime (data) {
+    const keys = Object.keys(data);
+    for (const key of keys) {
+        if (key === 'seconds') {
+            console.log(data[key]);
+            const currentTime = data[key];
+            localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+        }
+    }
+}
 
-        player.on('play', function() {
-            console.log('played the video!');
-        });
+// Всстановлення часу початку відтворення відео в плейер
+player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0.0)
+    .then(function (seconds) {
+        seconds = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+}
+);
+
